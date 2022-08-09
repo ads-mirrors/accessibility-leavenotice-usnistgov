1a0e759687ef6757383d11380301244573ac2e25
Steve Barber <steve.barber@nist.gov>
2022-08-09 16:59:05 -0400

    jquery 3.x deprecates .unload() so use .on()

--- uncompressed/jquery.leaveNotice-nist.js
+++ uncompressed/jquery.leaveNotice-nist.js
@@ -113,7 +113,7 @@
 				});
 				
 				// Clears the display when leaving the page to prevent it from showing upon returning.
-				$(window).unload(function(){
+				$(window).on('unload', function(){
 					closeDialog(options, leaveIn);
 				});
 				
